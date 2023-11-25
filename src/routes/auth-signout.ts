b5e6975498642ec5f12905edc92d34ad36db82a3
Jianghao Wu <wujianghao0706@gmail.com>
2023-11-25 01:02:44 -0500

    Update auth-signout.ts

--- src/routes/auth-signout.ts
+++ src/routes/auth-signout.ts
@@ -1,12 +1,23 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
+import { Request, Response } from 'express';
+import session, { SessionData } from 'express-session';
+
+// Define a custom interface for session data
+interface MySession extends SessionData {
+  user?: {
+    _id: string;
+    email: string;
+  };
+}
 
 const router = express.Router();
 
-router.get('/api/auth/signout', (req: Request, res: Response) => {
-    
-    req.session.jwt = undefined;
+router.post('/api/auth/signout', (req: Request & { session: MySession }, res: Response) => {
+  // Clear the user information from the session
+  req.session.user = undefined;
 
-    res.send({});
+  res.status(200).send({ message: 'Sign-out successful' });
+});
 
 })
 
