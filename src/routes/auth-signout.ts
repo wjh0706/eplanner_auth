064492b32a8321511ad7d2dacb76e7392e826d13
Jianghao Wu <wujianghao0706@gmail.com>
2023-11-25 00:53:27 -0500

    v0.76

--- src/routes/auth-signout.ts
+++ src/routes/auth-signout.ts
@@ -1,13 +1,27 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
+import session from 'express-session';
 
 const router = express.Router();
 
+// Use the express-session middleware
+router.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 router.get('/api/auth/signout', (req: Request, res: Response) => {
-    
-    req.session.jwt = undefined;
+  // Destroy the session to sign out the user
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
 
+    res.clearCookie('your-session-cookie-name'); // Clear the session cookie
     res.send({});
+  });
+});
 
-})
-
-export { router as SingOutRouter };
+export { router as SignOutRouter };
--- src/routes/auth-user.ts
+++ src/routes/auth-user.ts
@@ -1,11 +1,49 @@
-import express, { Request, Response } from 'express';
-import { currentUser } from '../utils/current-user';
+// import express, { Request, Response } from 'express';
+// import { currentUser } from '../utils/current-user';
+
+// const router = express.Router();
+
+// router.get('/api/auth/user', currentUser, (req: Request, res: Response) => {
+
+//     res.send({ currentUser: req. currentUser || null })
+// })
+
+// export { router as UserRouter };
+import express from 'express';
+import { Request, Response } from 'express';
+import { User } from '../models/user-model';
+import { SessionData } from 'express-session';
+
+// Define a custom interface for session data
+interface MySession extends SessionData {
+  user?: {
+    _id: string;
+    email: string;
+  };
+}
 
 const router = express.Router();
 
-router.get('/api/auth/user', currentUser, (req: Request, res: Response) => {
+router.get('/api/auth/user', async (req: Request & { session: MySession }, res: Response) => {
+  // Check if user is authenticated
+  if (!req.session || !req.session.user) {
+    // User is not authenticated
+    return res.status(401).send({ message: 'Unauthorized' });
+  }
 
-    res.send({ currentUser: req. currentUser || null })
-})
+  // User is authenticated, retrieve user details
+  const { _id, email } = req.session.user;
+
+  // Fetch additional user data from the database if needed
+  const currentUser = await User.findById(_id);
+
+  if (!currentUser) {
+    return res.status(404).send({ message: 'User not found' });
+  }
+
+  // Return user details
+  res.status(200).send({ _id: currentUser._id, email: currentUser.email });
+});
 
 export { router as UserRouter };
+
