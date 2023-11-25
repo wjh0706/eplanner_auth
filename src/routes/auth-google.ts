3f1957add8ad01abe7cfe3cfd991013516514253
Jianghao Wu <wujianghao0706@gmail.com>
2023-11-25 01:41:50 -0500

    Update auth-google.ts

--- src/routes/auth-google.ts
+++ src/routes/auth-google.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response, NextFunction } from "express";
 import passport from "passport";
-import session from "express-session";
+import session, { SessionData } from "express-session";
 import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
-import { v4 as uuid } from 'uuid'; 
+import { v4 as uuid } from "uuid";
+import { Email } from "../utils/email";
+import { User } from "../models/user-model";
+import { Password } from "../utils/password";
 
 const router = express.Router();
-
+interface MySession extends SessionData {
+  user?: {
+    _id: string;
+    email: string;
+  };
+}
 // Configure session middleware
 router.use(
   session({
@@ -46,7 +54,10 @@ passport.deserializeUser((obj: any, done) => {
 router.use(passport.initialize());
 router.use(passport.session());
 
-router.get('/api/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get(
+  "/api/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 // Google callback route
 router.get(
@@ -59,16 +70,24 @@ router.get(
 );
 
 // Get user's email route
-router.get(
+router.post(
   "/api/auth/getemail",
-  (req: Request, res: Response, next: NextFunction) => {
+  async (
+    req: Request & { session: MySession },
+    res: Response,
+    next: NextFunction
+  ) => {
     // Check if the user is authenticated
     if (!req.isAuthenticated()) {
       return res.status(401).json({ error: "Not authenticated" });
     }
 
     // Extract the user from the session
-    const user = req.user as { profile: Profile; accessToken: string; refreshToken: string };
+    const user = req.user as {
+      profile: Profile;
+      accessToken: string;
+      refreshToken: string;
+    };
 
     // Now you can access properties without TypeScript complaining
     const email = user?.profile?.emails?.[0]?.value;
@@ -77,8 +96,33 @@ router.get(
       return res.status(400).json({ error: "Email not found in user profile" });
     }
 
+    const encryptedEmail = await Email.encryptEmail(email);
+
+    // check if Email is already in use.
+    const userExists = await User.findOne({
+      email: encryptedEmail,
+    });
+    if (userExists) {
+      req.session.user = {
+        _id: userExists._id,
+        email: userExists.email,
+      };
+      const user = req.session.user as MySession["user"];
+      res.status(200).send(userExists);
+    } else {
+      const hashedPassword = await Password.toHash("DefaultPwd1");
+      const user = new User({
+        email: encryptedEmail,
+        password: hashedPassword,
+        isVerified: true,
+      });
+      await user.save();
+      res.status(201).send(user);
+    }
+
     // Respond with the user's email
-    res.json({ email });
+    //res.json({ email });
+    res.redirect("/api/auth/user");
   }
 );
 
