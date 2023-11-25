1e6ad95590a103cb110eef2acbc818aff5e95e3d
Jianghao Wu <wujianghao0706@gmail.com>
2023-11-25 00:24:19 -0500

    Update express.d.ts

--- src/utils/express.d.ts
+++ src/utils/express.d.ts
@@ -1,21 +1,21 @@
-// import "express-session";
-// import jwt from "jsonwebtoken";
+import "express-session";
+import jwt from "jsonwebtoken";
 
-// interface UserPayload {
-//     id: string;
-//     email: string;
-// }
+interface UserPayload {
+    id: string;
+    email: string;
+}
 
-// declare module "express-session" {
-//   interface Session {
-//     jwt?: string;
-//   }
-// }
+declare module "express-session" {
+  interface Session {
+    jwt?: string;
+  }
+}
 
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       currentUser?: UserPayload;
-//     }
-//   }
-// }
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
